fbc16890712c9a346493a410d46e5194506abe7d
Alejandro <alexbuesa12@gmail.com>
2018-11-13 17:44:06 +0100

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+## Código fuente del proyecto
+
+´´´
+npm install
+```
+
+# Notas personales
+"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath "C:\MongoData\db"
---
+++ server/server.js
@@ -0,0 +1,24 @@
+require("./config/config");
+//require('bootstrap');
+
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, "../public")));
+console.log(path.resolve(__dirname, "../public"));
+
+//Configuración global de rutas
+app.use(require("./routes/index"));
+
+app.listen(process.env.PORT, () => console.log("Escuchando puerto: ", process.env.PORT));
